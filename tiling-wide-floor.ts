9798313d692263600aa8f1a68a6d510343188a55
Chuabacca <jchua.yf@gmail.com>
2025-11-25 16:53:17 -0800

    add tiling wide floor

---
+++ tiling-wide-floor.ts
@@ -0,0 +1,21 @@
+// # Tiling Wide Floor
+
+// Given `n >= 1`, find the number of different ways to cover a `3xn` floor with `3x1` and `1x3` tiles.
+
+// Example 1: n = 6
+// Output: 6
+
+// Example 2: n = 1
+// Output: 1
+
+// Example 3: n = 2
+// Output: 1
+
+// The following image shows the 6 different ways to cover a `3x6` floor with `3x1` and `1x3` tiles.
+
+// https://iio-beyond-ctci-images.s3.us-east-1.amazonaws.com/recursion-fig10.png
+
+// Constraints:
+
+// - `1 ≤ n ≤ 45`
+// - Assume that the solution will fit in a signed `64`-bit integer.
